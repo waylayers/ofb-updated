2154a131267e10a3423761f90b71e1910b4a38f4
mallusrgreat <mallusrgreat@duck.com>
2024-09-17 01:09:05 +0530

    directions in logs

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -8,7 +8,14 @@ export default new SelfbotEvent({
     selfbot.logger.info(`Connected as ${selfbot.user?.tag}`);
     if (selfbot.config.settings.autoStart && !selfbot.status.started) {
       farm(selfbot);
+    } else {
+      selfbot.logger.info(
+        `Start the bot by entering "start" command here or in the discord server as "${selfbot.config.prefix}start".`
+      );
     }
+    selfbot.logger.info(
+      `You can pause the bot any time using "${selfbot.config.prefix}pause" and resume using "${selfbot.config.prefix}resume". You can also use these commands in here (in the command line), without including the prefix.\nThe bot will automatically pause whenever there is a captcha, so stay tuned in for notifications regarding captcha and solve them accordingly in order to not get banned.`
+    );
     selfbot.updateRPC();
   },
 });
